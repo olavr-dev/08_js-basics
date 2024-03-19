1777a3b25b440fc08d42f51d9181ee840599f88b
Olav Rorvik <olavr.dev@gmail.com>
2024-03-19 20:38:26 +0100

    first operation

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let age = 42;
 let userName = 'Olav';
-let hobbies = ['Web Design', 'Gaming', 'Programming'];
+let hobbies = ['Web Design', 'Gaming', 'Cooking'];
 let job = {
   title: 'Web Developer',
   location: 'Sandnes',
   salary: 750000,
 };
 
-// Targeting a value inside an array
+let adultYears = age - 18;
+
 console.log('------------------------------------');
-console.log(hobbies[0]);
-console.log('------------------------------------');
-// Targeting a property inside an object
-console.log('------------------------------------');
-console.log(job.title);
+console.log(adultYears);
 console.log('------------------------------------');
