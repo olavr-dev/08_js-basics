76e0ec1224711dce8557c2815499f58276059af5
Olav Rorvik <olavr.dev@gmail.com>
2024-03-19 19:10:20 +0100

    move js to separate file

---
+++ script.js
@@ -0,0 +1,15 @@
+// Define variables
+let age = 42;
+let greetingText = "Hi, I'm Olav";
+
+// Show in browser
+alert(greetingText);
+console.log(greetingText);
+
+// Set a new value for the variable
+greetingText = 'My last name is Øye Rørvik';
+
+// Show in browser
+console.log(greetingText);
+alert(age);
+console.log(age);
