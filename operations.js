c9f1ad46844ce7ced7cff675e2e04984ab2e5eaf
Olav Rorvik <olavr.dev@gmail.com>
2024-03-20 17:33:10 +0100

    learning about math operations

---
+++ operations.js
@@ -0,0 +1,23 @@
+// Math operations
+
+console.log(10 + 4); // 14
+console.log(10 - 4); // 6
+console.log(10 * 4); // 40
+console.log(10 / 4); // 2.5
+console.log(10 % 4); // 2
+console.log(10 % 3); // 1
+
+console.log(10 + 3 - 5 * 10); // -37
+
+console.log((10 + 3 - 5) * 10); // 80
+
+let result = (10 + 3 - 5) * 10; // Stores the result in a variable
+result = 10 * 4; // Overwrites the result variable (Sets it to 40)
+
+result++; // same as result = result + 1
+result--; // same as result = result - 1
+
+result += 5; // same as result = result + 5
+result -= 5; // same as result = result - 5
+result *= 5; // same as result = result * 5
+result /= 5; // same as result = result / 5
