8d90dda322a8f65d78e8a115566501634101551c
Olav Rorvik <olavr.dev@gmail.com>
2024-03-20 10:29:42 +0100

    exercise complete

--- exercise.js
+++ exercise.js
@@ -8,9 +8,25 @@
 //    - A variable that stores the name of an online course of your choice
 //    - A variable that stores the price of that course
 //    - A variable that stores the three main goals that you have, when taking this course
+
+let courseName = '100 Days of Code';
+let coursePrice = 169;
+let mainGoals = ['Learn programming', 'Work from home', 'Better salary'];
+
 // 2) Output ("alert") the three variable values
+
+console.log(courseName);
+console.log(coursePrice);
+console.log(mainGoals);
+
 // 3) Try "grouping" the three variables together and still output their values thereafter
+
+console.log(courseName, coursePrice, mainGoals);
+
 // 4) Also output the second element in your "main goals" variable
+
+console.log(mainGoals[1]);
+
 // 5) Add a custom command that does the following:
 //    - Use your "main goals" variable and access an element by its identifier
 //    - The concrete identifier value should be dynamic / flexible
@@ -18,4 +34,11 @@
 //    - The "main goals" variable should also be dynamic: The command should work
 //      with ANY list of values
 //    - The custom command should provide the accessed value (i.e. the list element)
+
+function goals() {
+  return mainGoals;
+}
+
 // 6) Execute your custom command from (5) and output ("alert") the result
+
+console.log(goals(1));
