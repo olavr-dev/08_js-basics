57236eb711f066c53d4a63f5865c25281a4eeafc
Olav Rorvik <olavr.dev@gmail.com>
2024-03-20 18:07:56 +0100

    learning math and string operations

--- operations.js
+++ operations.js
@@ -11,13 +11,14 @@ console.log(10 + 3 - 5 * 10); // -37
 
 console.log((10 + 3 - 5) * 10); // 80
 
-let result = (10 + 3 - 5) * 10; // Stores the result in a variable
-result = 10 * 4; // Overwrites the result variable (Sets it to 40)
+// String operations
 
-result++; // same as result = result + 1
-result--; // same as result = result - 1
+console.log('Olav' + ' ' + 'Øye Rørvik');
 
-result += 5; // same as result = result + 5
-result -= 5; // same as result = result - 5
-result *= 5; // same as result = result * 5
-result /= 5; // same as result = result / 5
+let userName = 'Olav';
+
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ['Gaming', 'Cooking'];
+console.log(hobbies.length);
